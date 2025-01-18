7b12268b1ce39050247fec33231a863d829d3055
miraili <miraili61@gmail.com>
2025-01-18 03:37:41 +0300

    timescale changed

--- Assets/Scripts/EnterPoint.cs
+++ Assets/Scripts/EnterPoint.cs
@@ -10,14 +10,8 @@ public class EnterPoint : MonoBehaviour
 {
     [SerializeField] private Settings _settings;
     [SerializeField] private Canvas _targetCanvas;
-    private float _timeScale = 1;
+    private float _timeScale = 5;
     
-    // Privet, ya bil zdes!
-
-    /*/
-     * qweqwe
-     * qweqweqwrq
-    /*/ 
     void Start()
     {
         Time.timeScale = _timeScale;
